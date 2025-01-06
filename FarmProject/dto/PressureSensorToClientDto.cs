a4238e0fbe984d9ef1cbcd6ad19fae0f8590f1ab
Weed2004 <weed4sgv@gmail.com>
2025-01-06 18:22:27 +0300

    add sensor to client dto

---
+++ FarmProject/dto/PressureSensorToClientDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FarmProject.dto
+{
+    public class PressureSensorToClientDto
+    {
+        public required string Imei { get; set; }
+        public required string Gps { get; set; }
+        public required List<PressureMeasurementsToClientDto> Measurements { get; set; }
+        public required PressureSensorSettingsToClientDto Settings { get; set; }
+    }
+}
--- FarmProject/dto/servisces/PressureSensorDtoConvertService.cs
+++ FarmProject/dto/servisces/PressureSensorDtoConvertService.cs
@@ -2,7 +2,8 @@
 
 namespace FarmProject.dto.servisces
 {
-    public class PressureSensorDtoConvertService
+    public class PressureSensorDtoConvertService(PressureMeasurmentsDtoConvertService measurementsConverter,
+            PressureSettingsDtoConvertService settingsConverter)
     {
         public PressureSensor ConvertToMoodel(PressureSensorFromClientDto sensor)
         {
@@ -11,6 +12,17 @@ namespace FarmProject.dto.servisces
                 GPS = sensor.Gps,
                 IMEI = sensor.Imei,
                 Settings = new() { IMEI = sensor.Imei }
+            };
+        }
+
+        public PressureSensorToClientDto ConvertToClient(PressureSensor sensor)
+        {
+            return new PressureSensorToClientDto()
+            {
+                Gps = sensor.GPS,
+                Imei = sensor.IMEI,
+                Settings = settingsConverter.ConvertToClient(sensor.Settings),
+                Measurements = sensor.Measurements.Select(m => measurementsConverter.ConvertToClientDto(m)).ToList()
             };
         }
     }
