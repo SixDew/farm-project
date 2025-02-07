966e9c259f88427d784f0d30662bb192d70ab130
SixDew <weed4shv@gmail.com>
2025-02-07 14:00:34 +0300

    reforged sensors api

--- FarmProject/client-app/src/sensors/api/sensors-api.js
+++ FarmProject/client-app/src/sensors/api/sensors-api.js
@@ -1,13 +1,20 @@
 export async function getAllPressureSensors(){
-    const response = await fetch('https://localhost:7061/sensors/pressure')
+    return await getRequest(`${serverUrl}/sensors/pressure`)
+}
+
+export async function getPressureMeasurements(imei){
+    return await getRequest(`${serverUrl}/sensors/pressure/measurements/${imei}`)
+}
+
+export async function getPressureSensorData(imei){
+    return await getRequest(`${serverUrl}/sensors/pressure/${imei}`)
+}
+
+async function getRequest(url){
+    const response = await fetch(url)
     if(response.ok){
         return await response.json()
     }
 }
 
-export async function getPressureMeasurements(imei){
-    const response = await fetch(`https://localhost:7061/sensors/pressure/measurements/${imei}`)
-    if(response.ok){
-        return await response.json()
-    }
-}
+const serverUrl = 'https://localhost:7061'
