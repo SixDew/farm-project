e37e678fce967a944c88b14350c947f66d91be63
SixDew <weed4shv@gmail.com>
2025-03-13 10:25:10 +0300

    create measurements add logs

--- FarmProject/mqtt/services/MqttBrokerService.cs
+++ FarmProject/mqtt/services/MqttBrokerService.cs
@@ -12,9 +12,11 @@ public class MqttBrokerService(IServiceProvider _serviceProvider)
 {
     public async Task InterceptingPublishAsync(InterceptingPublishEventArgs e)
     {
+        Console.WriteLine("Обработка сообщения");
         using (var scope = _serviceProvider.CreateScope())
         {
             string topic = e.ApplicationMessage.Topic;
+            Console.WriteLine("Топик сообщение: " + topic);
             var sensorProvider = scope.ServiceProvider.GetRequiredService<PressureSensorProvider>();
 
             switch (topic)
@@ -23,18 +25,32 @@ public class MqttBrokerService(IServiceProvider _serviceProvider)
                     {
                         try
                         {
+                            Console.WriteLine("Добавление измерения");
+
                             var data = JsonSerializer.Deserialize<PressureMeasurementsFromSensorDto>(Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
+
+                            Console.WriteLine($"""
+                                IMEI: {data.IMEI},
+                                Измерение 1: {data.PRR1},
+                                Измерение 2: {data.PRR2}
+                                """);
 
                             var validationService = scope.ServiceProvider.GetRequiredService<PressureValidationService>();
                             var dtoConverter = scope.ServiceProvider.GetRequiredService<PressureMeasurmentsDtoConvertService>();
 
+                            Console.WriteLine("Валидация");
                             if (await validationService.IsValidatedAsync(data.IMEI))
                             {
+                                Console.WriteLine("Поиск датчика");
                                 var sensorMeasurementsList = await sensorProvider.GetMeasurmentsByImeiAync(data.IMEI);
 
+                                Console.WriteLine("Конвертация");
                                 var measurementsModel = dtoConverter.ConvertToModel(data);
+                                Console.WriteLine("Добавление");
                                 sensorMeasurementsList.Add(measurementsModel);
+                                Console.WriteLine("Сохранение");
                                 await sensorProvider.SaveChangesAsync();
+                                Console.WriteLine("Завершение");
                             }
                         }
                         catch (JsonException ex)
