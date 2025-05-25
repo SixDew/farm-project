76714d22e109305c1851e8565d19c77e8823d26f
SixDew <weed4shv@gmail.com>
2025-05-25 12:22:24 +0300

    improved alarm info container and group dialog design

--- FarmProject/client-app/src/sensors/AlarmNotificationsInfo.tsx
+++ FarmProject/client-app/src/sensors/AlarmNotificationsInfo.tsx
@@ -2,6 +2,8 @@ import { PressureAlarmDto } from "../interfaces/DtoInterfaces"
 import AlarmNotification from "./AlarmNotification"
 import './AlarmNotificationsInfo.css'
 
+import niceImage from '../images/nice.png'
+
 interface AlarmNotificationsInfoProps{
     alarmedMeasurements:PressureAlarmDto[],
     onNotificationCheck?:(id:number)=>void
@@ -9,12 +11,21 @@ interface AlarmNotificationsInfoProps{
 
 export default function AlarmNotificationsInfo({alarmedMeasurements,onNotificationCheck}:AlarmNotificationsInfoProps){
     return (
-        <div className="notifucations-info-container">
-            <h3>Предупреждения</h3>
-            {alarmedMeasurements.map((n, index)=><AlarmNotification measurement1={n.measurement1} measurement2={n.measurement2} date={n.measurementsTime} key={index}
-            onCheck={()=>{
-                onNotificationCheck && onNotificationCheck(n.id)
-            }}/>)}
-        </div>
+        <>
+            {
+                alarmedMeasurements.length > 0 ?
+                <div className="notifications-info-container">
+                    <h3>Предупреждения</h3>
+                    {alarmedMeasurements.map((n, index)=><AlarmNotification measurement1={n.measurement1} measurement2={n.measurement2} date={n.measurementsTime} key={index}
+                    onCheck={()=>{
+                        onNotificationCheck && onNotificationCheck(n.id)
+                    }}/>)}
+                </div>:
+                <div className="empty-alarm-info-container">
+                    <h3>Аварийных измерений не обнаружено</h3>
+                    <img src={niceImage} width="64px" height="64px"></img>
+                </div>
+            }
+        </>
     )
 }
